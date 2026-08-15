uibcdf/molsysmt

chore(units): adopt the shared MolSysSuite unit policy
The suite libraries share one PyUnitWizard kernel inside a process and each
configured it from its own `_pyunitwizard.py`, which runs on first import. The
last import therefore won, and the four did not agree: mass was `dalton` here
and `amu` there, angles `radians` or `degrees`, energy `kJ/mol` or `kcal/mol`.
The same call returned 1.0 radian or 57.3 degree depending on import order, and
with lazy imports "order" could mean which notebook cell ran first.

Two changes, together:

All four now declare the same policy -- MolSysMT's -- so whoever gets there
first no longer matters. And each applies it only when no policy is active, so
a later first-import cannot silently undo what another library, or the user,
already decided. `provenance` records who set it, readable through
`puw.configure.report()`.

Fast tracks stay unconditional: `to_nanometers` means nanometers whatever the
active standard units are, so they are named converters rather than policy.

Suite-wide decision recorded in `pyunitwizard/devguide/pending_proposals/
molsyssuite_unit_configuration_authority.md`. Requires the `has_active_policy()`
and `provenance` support added in pyunitwizard.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>

molsysmt/_pyunitwizard.py
@@ -1,14 +1,40 @@
 # Configure PyUnitWizard
+#
+# The MolSysSuite libraries share one PyUnitWizard kernel inside a process, so
+# whichever of them configured it last used to win -- and with lazy imports
+# "last" can be a notebook cell run much later, silently undoing what the user
+# chose. They therefore all declare the *same* policy, and each one applies it
+# only when no policy is active yet.
+#
+# The shared policy is MolSysMT's. Changing it is a suite-wide decision, not a
+# per-library one: see `pyunitwizard/devguide/pending_proposals/
+# molsyssuite_unit_configuration_authority.md`.
 
 import pyunitwizard as puw
 
-puw.configure.set_default_form('pint')
-puw.configure.set_default_parser('pint')
-puw.configure.set_standard_units(['nm', 'ps', 'K', 'mole', 'dalton', 'e',
-                                 'kJ/mol', 'kJ/(mol*nm)', 'kJ/(mol*nm**2)', 'radians'])
-# puw.configure.add_constant(name, value, unit)
+STANDARD_UNITS = [
+    'nm',              # length: nanometer
+    'ps',              # time: picosecond
+    'K',               # temperature: kelvin
+    'mole',            # amount of substance
+    'dalton',          # mass
+    'e',               # charge: elementary charge
+    'kJ/mol',          # energy
+    'kJ/(mol*nm)',     # force
+    'kJ/(mol*nm**2)',  # force constant
+    'radians',         # angle
+]
 
-# Registering MolSysMT canonical fast-tracks
-puw.register_fast_track("nanometers", puw.unit("nm"))
-puw.register_fast_track("picoseconds", puw.unit("ps"))
-puw.register_fast_track("kelvin", puw.unit("K"))
+# Only when nobody has decided yet. An active policy belongs to whoever set it:
+# another suite library, or the user.
+if not puw.configure.has_active_policy():
+    puw.configure.set_default_form('pint')
+    puw.configure.set_default_parser('pint')
+    puw.configure.set_standard_units(STANDARD_UNITS, provenance='molsysmt')
+
+# Fast tracks are named converters, not policy: `to_nanometers` means
+# nanometers whatever the active standard units are. Registering them is
+# idempotent across the suite, so it is unconditional.
+puw.register_fast_track('nanometers', puw.unit('nm'))
+puw.register_fast_track('picoseconds', puw.unit('ps'))
+puw.register_fast_track('kelvin', puw.unit('K'))

uibcdf/topomt

topomt/_pyunitwizard.py
@@ -1,24 +1,40 @@
 # Configure PyUnitWizard
+#
+# The MolSysSuite libraries share one PyUnitWizard kernel inside a process, so
+# whichever of them configured it last used to win -- and with lazy imports
+# "last" can be a notebook cell run much later, silently undoing what the user
+# chose. They therefore all declare the *same* policy, and each one applies it
+# only when no policy is active yet.
+#
+# The shared policy is MolSysMT's. Changing it is a suite-wide decision, not a
+# per-library one: see `pyunitwizard/devguide/pending_proposals/
+# molsyssuite_unit_configuration_authority.md`.
 
 import pyunitwizard
 
-standards = [
-    'nm',      # length: nanometer
-    'ps',      # time: picosecond
-    'K',       # temperature: kelvin
-    'mole',    # amount: mole
-    'dalton',  # mass
-    'e',       # charge: elementary charge
-    'kJ/mol',  # energy: kilojoules/mole
-    'kJ/(mol*nm)',
-    'kJ/(mol*nm**2)',
-    'radians',
+STANDARD_UNITS = [
+    'nm',              # length: nanometer
+    'ps',              # time: picosecond
+    'K',               # temperature: kelvin
+    'mole',            # amount of substance
+    'dalton',          # mass
+    'e',               # charge: elementary charge
+    'kJ/mol',          # energy
+    'kJ/(mol*nm)',     # force
+    'kJ/(mol*nm**2)',  # force constant
+    'radians',         # angle
 ]
 
-pyunitwizard.configure.set_default_form('pint')
-pyunitwizard.configure.set_default_parser('pint')
-pyunitwizard.configure.set_standard_units(standards)
+# Only when nobody has decided yet. An active policy belongs to whoever set it:
+# another suite library, or the user.
+if not pyunitwizard.configure.has_active_policy():
+    pyunitwizard.configure.set_default_form('pint')
+    pyunitwizard.configure.set_default_parser('pint')
+    pyunitwizard.configure.set_standard_units(STANDARD_UNITS, provenance='topomt')
+
+# Fast tracks are named converters, not policy: `to_nanometers` means
+# nanometers whatever the active standard units are. Registering them is
+# idempotent across the suite, so it is unconditional.
 pyunitwizard.register_fast_track('nanometers', pyunitwizard.unit('nm'))
 pyunitwizard.register_fast_track('picoseconds', pyunitwizard.unit('ps'))
 pyunitwizard.register_fast_track('kelvin', pyunitwizard.unit('K'))
-